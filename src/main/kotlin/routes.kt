ca3f98d62403d4dc3413ff8c296b40c175690526
Ashok Gelal <ashokgelal@gmail.com>
2019-12-14 13:53:28 -0700

    Test auth

--- src/main/kotlin/routes.kt
+++ src/main/kotlin/routes.kt
@@ -1,15 +1,18 @@
 package dev.alpas.alpasdev
 
 import dev.alpas.alpasdev.controllers.DocsController
+import dev.alpas.alpasdev.controllers.HomeController
 import dev.alpas.alpasdev.controllers.WelcomeController
+import dev.alpas.auth.authRoutes
 import dev.alpas.routing.Router
 
 fun Router.addRoutes() = apply {
     webRoutes()
+    this.authRoutes()
 }
 
 private fun Router.webRoutes() {
-    get("/", WelcomeController::class).name("welcome")
+    get("/", HomeController::class).name("welcome")
     get("/docs/<page>", DocsController::class, "show").name("docs.show")
     get("/docs", DocsController::class, "index").name("docs.index")
 }
